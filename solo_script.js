eead0f9c1e244abe284bf9b72d3ec34346b4a31b
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-10-27 14:23:06 -0500

    fixed 3 bugs

--- solo_script.js
+++ solo_script.js
@@ -16,9 +16,10 @@ position = document.getElementById('content');
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
 for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array);
+	array[i] = calculateSTI(array[i]);
  	newEl = document.createElement('li');
 	newText = document.createTextNode(array[i]);
+	console.log(array[i]);
 	newEl.appendChild(newText);
 	position.appendChild(newEl);
 }
@@ -38,8 +39,8 @@ function calculateSTI(array){
   }
 
   newArray[1] = bonus; // percentage of STI employee to receive
-  newArray[2] = baseSalary * (1.0 + bonus); // adjusted annual compensation
-  newArray[3] = baseSalary * bonus; //total bonus rounded to nearest dollar
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus)); // adjusted annual compensation
+  newArray[3] = Math.round(baseSalary * bonus); //total bonus rounded to nearest dollar
   console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
   return newArray;
 }
@@ -63,7 +64,8 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent - 1;
+	console.log(basePercent);
+  return basePercent;
 }
 
 function getYearAdjustment(employeeNumber){

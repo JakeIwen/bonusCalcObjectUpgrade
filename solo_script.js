c270642f26ecc108ba50c952a340e83076f6cc43
jakeiwen <iwenx006@umn.edu>
2016-10-28 10:38:46 -0500

    edit three_bugs to convert employee-property-arrays into Person Objects and use those

--- solo_script.js
+++ solo_script.js
@@ -8,31 +8,42 @@ var arrayScout = ["Scout", "6243", "74750", 5];
 
 var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
 
+//build object, convert array elements
+
+function Person(oneEmployeeArray) {
+	this.name = oneEmployeeArray[0];
+	this.empNum = oneEmployeeArray[1];
+	this.oldSalary = oneEmployeeArray[2];
+	this.rScore= oneEmployeeArray[3];
+}
+
+//turn array of arrays into array of Person-objects
+for (var i = 0; i < array.length; i++) {
+	array[i] = new Person(array[i]);
+}
+
 //Create variables used to write to the DOM
 var newEl, newText, position;
 //Capture the position of insertion into the DOM
 position = document.getElementById('content');
 
 //Loop the array, extracting each array and writing information to the DOM
-//Note that the information is not 'clean'
+//Note that the information is not 'clean''
 for(var i = 0; i < array.length; i++){
 	array[i] = calculateSTI(array[i]);
  	newEl = document.createElement('li');
 	newText = document.createTextNode(array[i]);
-	console.log(array[i]);
 	newEl.appendChild(newText);
 	position.appendChild(newEl);
 }
 
-function calculateSTI(array){
+//edit function to take in Person-object and address object properties rather than array indicies
+function calculateSTI(onePerson){
   var newArray = [];
-
-  newArray[0] = array[0]; // employee name
-
-  var employeeNumber = array[1];
-  var baseSalary = array[2];
-  var reviewScore = array[3];
-
+  newArray[0] = onePerson.name; // employee name
+  var employeeNumber = onePerson.empNum;
+  var baseSalary = onePerson.oldSalary;
+  var reviewScore = onePerson.rScore;
   var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
   if(bonus > 0.13){
     bonus = 0.13;
@@ -41,7 +52,7 @@ function calculateSTI(array){
   newArray[1] = bonus; // percentage of STI employee to receive
   newArray[2] = Math.round(baseSalary * (1.0 + bonus)); // adjusted annual compensation
   newArray[3] = Math.round(baseSalary * bonus); //total bonus rounded to nearest dollar
-  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+  //console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
   return newArray;
 }
 
